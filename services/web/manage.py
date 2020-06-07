da6f1f180eef666ff0269a73be1d8f13b530b72c
Norman Wong <nich19032@outlook.com>
2020-06-08 01:33:50 +1000

    Setup postgres db service

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,10 +1,16 @@
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db
 
 
 cli = FlaskGroup(app)
 
+# registers a new command, create_db, to the CLI so that we can run it from the command line
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == "__main__":
     cli()
