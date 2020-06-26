0f0815ea263e03ad86e4ccef5825f7f3579174f1
Norman Wong <nich19032@outlook.com>
2020-06-26 21:39:37 +1000

    Deleted volume environment variables for development

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -6,8 +6,6 @@ APP_NAME = "flask-react-spa"
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
-    MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
     SECRET_KEY = "my_precious"
 
 
@@ -23,3 +21,5 @@ class TestingConfig(BaseConfig):
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
+    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
+    MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
