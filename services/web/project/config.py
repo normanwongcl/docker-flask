05bd66ddbfe195d12be99f1f8010044f38c04617
Norman Wong <nich19032@outlook.com>
2020-07-03 01:43:42 +1000

    Added config unit test

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -4,20 +4,24 @@ APP_NAME = "flask-react-spa"
 
 
 class BaseConfig:
+    FLASK_APP = f"{os.getenv('APP_FOLDER')}/project/__init__.py"
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = True
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
+    DEBUG = True
 
 
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
     MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
+    DEBUG = False
