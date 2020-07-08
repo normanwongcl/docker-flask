c2ff2b714be2c0e26471b1468ea63fba6d075103
Norman Wong <nich19032@outlook.com>
2020-07-08 14:26:24 +1000

    Refactor docker build command in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+TAG_NAME="$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com/docker-flask:prod
+
 echo building and testing dev images...
 
 docker-compose up -d --build
@@ -12,6 +14,6 @@ docker-compose exec -T web /bin/sh -c "isort project/*/*.py --check-only"
 echo building prod images...
 
 docker build \
+    -t "$TAG_NAME" \
     -f services/web/Dockerfile.prod \
-    -t "$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com/docker-flask:prod \
     ./services/web
